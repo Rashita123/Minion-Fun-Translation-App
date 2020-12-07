756c7452fb6f910a5cd81343b9b305747b2be0bf
Rashita123 <rrashita_be19@thapar.edu>
2020-12-07 16:13:14 +0530

    styling and JS done

---
+++ app.js
@@ -0,0 +1,20 @@
+var button = document.querySelector("#button");
+var input = document.querySelector("#input-box");
+var output = document.querySelector("#output");
+
+var serverURL="https://api.funtranslations.com/translate/minion.json";
+
+function constructURL(text){
+    return serverURL+"?text="+"text";
+}
+
+var inputText=input.value;
+function clickHandler(){
+    fetch(constructURL(inputText))
+    .then (response => response.json())
+    .then(json => {
+        output.innerText=json.contents.translated;
+    })
+}
+
+button.addEventListener("click",clickHandler);
